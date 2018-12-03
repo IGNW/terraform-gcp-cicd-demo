4cdb59c228c741c54df3835abe95cc3fef9ffd2f
Fat fingers Walker <tylerw@ignw.io>
2018-12-03 12:24:48 -0600

    replaced template with set

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -2,17 +2,19 @@ provider "helm" {
     service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
 }
 
-data "template_file" "jenkins_values" {
-    template = <<EOF
-Master:
-  AdminUser: ${var.jenkins_user}
-  AdminPassword: ${var.jenkins_password}
-EOF
-}
 
 resource "helm_release" "jenkins" {
     name      = "jenkins"
     chart     = "stable/jenkins"
-    values    = "${data.template_file.jenkins_values.rendered}"
     depends_on = ["null_resource.helm_bootstrap"]
+
+		set {
+        name = "AdminUser"
+        value = ${var.jenkins_user}"
+    }
+
+		set {
+        name = "AdminPassword"
+        value = ${var.jenkins_password}"
+    }
 }
