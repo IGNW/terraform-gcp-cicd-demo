215001df40cd6a495bfdf2512c080b2194620c1f
Phil Taylor <phil.taylor@daimler.com>
2018-12-03 09:47:31 -0800

    (update) move to set blocks vs whole values.yaml

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -2,17 +2,19 @@ provider "helm" {
     service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
 }
 
-data "template_file" "jenkins_values" {
-    template = <<EOF
-Master:
-  AdminUser: ${var.jenkins_user}
-  AdminPassword: ${var.jenkins_password}
-EOF
-}
-
 resource "helm_release" "jenkins" {
     name      = "jenkins"
     chart     = "stable/jenkins"
-    values    = "${data.template_file.jenkins_values.rendered}"
+
+    set {
+        name    = "Master.AdminUser"
+        value   = "${var.jenkins_user}"
+    }
+
+    set {
+        name    = "Master.AdminPassword"
+        value   = "${var.jenkins_password}"
+    }
+
     depends_on = ["null_resource.helm_bootstrap"]
 }
