a556edb457137e97cfd618ef322564247c052974
Fat fingers Walker <tylerw@ignw.io>
2018-12-04 12:34:37 -0600

    Fixed spacing issues with xml credentials

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -16,27 +16,28 @@ resource "kubernetes_secret" "xmlcredentials" {
     }
   data {
     credentials.xml =<<EOF
-		<?xml version='1.1' encoding='UTF-8'?>
-      <com.cloudbees.plugins.credentials.SystemCredentialsProvider plugin="credentials@2.1.18">
-        <domainCredentialsMap class="hudson.util.CopyOnWriteMap$Hash">
-          <entry>
-            <com.cloudbees.plugins.credentials.domains.Domain>
-              <specifications/>
-            </com.cloudbees.plugins.credentials.domains.Domain>
-            <java.util.concurrent.CopyOnWriteArrayList>
-              <com.google.jenkins.plugins.credentials.oauth.GoogleRobotMetadataCredentials plugin="google-oauth-plugin@0.6">
-                <module class="com.google.jenkins.plugins.credentials.oauth.GoogleRobotMetadataCredentialsModule"/>
-                <projectId>ignw-internal-tools</projectId>
-              </com.google.jenkins.plugins.credentials.oauth.GoogleRobotMetadataCredentials>
-            </java.util.concurrent.CopyOnWriteArrayList>
-          </entry>
-        </domainCredentialsMap>
+<?xml version='1.1' encoding='UTF-8'?>
+<com.cloudbees.plugins.credentials.SystemCredentialsProvider plugin="credentials@2.1.18">
+  <domainCredentialsMap class="hudson.util.CopyOnWriteMap$Hash">
+    <entry>
+      <com.cloudbees.plugins.credentials.domains.Domain>
+        <specifications/>
+      </com.cloudbees.plugins.credentials.domains.Domain>
+      <java.util.concurrent.CopyOnWriteArrayList>
+        <com.google.jenkins.plugins.credentials.oauth.GoogleRobotMetadataCredentials plugin="google-oauth-plugin@0.6">
+          <module class="com.google.jenkins.plugins.credentials.oauth.GoogleRobotMetadataCredentialsModule"/>
+          <projectId>ignw-internal-tools</projectId>
+        </com.google.jenkins.plugins.credentials.oauth.GoogleRobotMetadataCredentials>
+      </java.util.concurrent.CopyOnWriteArrayList>
+    </entry>
+  </domainCredentialsMap>
+</com.cloudbees.plugins.credentials.SystemCredentialsProvider>
     EOF
     }
   }
 resource "helm_release" "jenkins" {
     name      = "jenkins"
     chart     = "stable/jenkins"
-		values = ["${data.template_file.values.rendered}"]
+    values = ["${data.template_file.values.rendered}"]
     depends_on = ["null_resource.helm_bootstrap", "kubernetes_secret.xmlcredentials"]
 }
